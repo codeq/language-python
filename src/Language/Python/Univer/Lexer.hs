cdf81c225a4b0380eee30c0ede987ecc1fc63c5e
Mike Yumatov <mike@yumatov.org>
2013-03-10 01:37:00 +0400

    Port lexer helpers from 3.* to univer

--- src/Language/Python/Univer/Lexer.hs
+++ src/Language/Python/Univer/Lexer.hs
@@ -14,7 +14,8 @@
 module Language.Python.Univer.Lexer (
    -- * Lexical analysis
    lex,
-   lexOneToken) where
+   lexOneToken
+) where
 
 import Prelude hiding (lex)
 import Language.Python.Univer.Parser.Lexer (lexToken, initStartCodeStack)
@@ -23,15 +24,16 @@ import Language.Python.Common.SrcLocation (initialSrcLocation)
 import Language.Python.Common.ParserMonad
        (ParseState (input), P, runParser, execParser, ParseError, initialState)
 
+initLexState :: String -> String -> ParseState
+initLexState input srcName = 
+   initialState (initialSrcLocation srcName) input initStartCodeStack
+
 -- | Parse a string into a list of Python Tokens, or return an error. 
 lex :: String -- ^ The input stream (python source code). 
     -> String -- ^ The name of the python source (filename or input device).
     -> Either ParseError [Token] -- ^ An error or a list of tokens.
 lex input srcName =
-   execParser lexer state
-   where
-   initLoc = initialSrcLocation srcName
-   state = initialState initLoc input initStartCodeStack
+   execParser lexer $ initLexState input srcName
 
 -- | Try to lex the first token in an input string. Return either a parse error
 -- or a pair containing the next token and the rest of the input after the token.
@@ -42,9 +44,7 @@ lexOneToken source srcName =
    case runParser lexToken state of
       Left err -> Left err
       Right (tok, state) -> Right (tok, input state)
-   where
-   initLoc = initialSrcLocation srcName
-   state = initialState initLoc source initStartCodeStack
+   where state = initLexState source srcName
 
 lexer :: P [Token]
 lexer = loop []
